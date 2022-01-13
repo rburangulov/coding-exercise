207a876734181f1e934e3a715623764be197f205
Ramil Burangulov <ramil.burangulov@helastel.com>
2022-01-13 18:16:08 +0400

    Change logic

--- client.py
+++ client.py
@@ -17,31 +17,11 @@ def send_message(message):
     conn.close()
 
 class  MyHandler(FileSystemEventHandler):
-    def on_modified(self,  event):
-        global access_points
-        updated_access_points = load_access_points(config.access_points_file)
-        for access_point in access_points["access_points"]:
-            is_found = False
-            for updated_access_point in updated_access_points["access_points"]:
-                if access_point["ssid"] == updated_access_point["ssid"]:
-                    is_found = True
-                    if access_point["snr"] != updated_access_point["snr"]:
-                        send_message("%s SNR has changed from %s to %s" % (access_point["ssid"], access_point["snr"], updated_access_point["snr"]))
-                    if access_point["channel"] != updated_access_point["channel"]:
-                        send_message("%s channel has changed from %s to %s" % (access_point["ssid"], access_point["channel"], updated_access_point["channel"]))
-            if is_found == False:
-                send_message("%s is removed from the list" % (access_point["ssid"]))
-        for updated_access_point in updated_access_points["access_points"]:
-            is_new = True
-            for access_point in access_points["access_points"]:
-                if updated_access_point["ssid"] == access_point["ssid"]:
-                    is_new = False
-            if is_new == True:
-                send_message("%s is added to the list with SNR %s and channel %s" % (updated_access_point["ssid"], updated_access_point["snr"], updated_access_point["channel"]))
-        access_points = updated_access_points
+    def on_modified(self, event):
+        send_message(load_access_points(config.access_points_file))
 
 if __name__ ==  "__main__":
-    access_points = load_access_points(config.access_points_file)
+    send_message(load_access_points(config.access_points_file))
     event_handler = MyHandler()
     observer = Observer()
     observer.schedule(event_handler,  path=config.access_points_file,  recursive=False)
--- server.py
+++ server.py
@@ -3,12 +3,38 @@ import config
 
 listener = Listener((config.server_address, config.server_port))
 
-while True:
-    conn = listener.accept()
+def calculate_changes(access_points, updated_access_points): 
+    for access_point in access_points["access_points"]:
+        is_found = False
+        for updated_access_point in updated_access_points["access_points"]:
+            if access_point["ssid"] == updated_access_point["ssid"]:
+                is_found = True
+                if access_point["snr"] != updated_access_point["snr"]:
+                    print("%s SNR has changed from %s to %s" % (access_point["ssid"], access_point["snr"], updated_access_point["snr"]))
+                if access_point["channel"] != updated_access_point["channel"]:
+                    print("%s channel has changed from %s to %s" % (access_point["ssid"], access_point["channel"], updated_access_point["channel"]))
+        if is_found == False:
+             print("%s is removed from the list" % (access_point["ssid"]))
+    for updated_access_point in updated_access_points["access_points"]:
+        is_new = True
+        for access_point in access_points["access_points"]:
+            if updated_access_point["ssid"] == access_point["ssid"]:
+                is_new = False
+        if is_new == True:
+            print("%s is added to the list with SNR %s and channel %s" % (updated_access_point["ssid"], updated_access_point["snr"], updated_access_point["channel"]))
+
+if __name__ ==  "__main__":
+    access_points = {}
     while True:
-        msg = conn.recv()
-        if msg == 'close connection':
-            conn.close()
-            break
-        print(msg)
+        conn = listener.accept()
+        while True:
+            msg = conn.recv()
+            if msg == 'close connection':
+                conn.close()
+                break
+            if access_points == {}:
+                access_points = msg
+            else:
+                calculate_changes(access_points, msg)
+                access_points = msg
 listener.close()
