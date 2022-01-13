257b29df480ad32b25c708abf0995b4c64d83c42
Ramil Burangulov <ingless1991@gmail.com>
2022-01-13 16:37:44 +0400

    Update client.py

--- client.py
+++ client.py
@@ -21,22 +21,22 @@ class  MyHandler(FileSystemEventHandler):
         global access_points
         updated_access_points = load_access_points(config.access_points_file)
         for access_point in access_points["access_points"]:
-            found = False
+            is_found = False
             for updated_access_point in updated_access_points["access_points"]:
                 if access_point["ssid"] == updated_access_point["ssid"]:
-                    found = True
+                    is_found = True
                     if access_point["snr"] != updated_access_point["snr"]:
                         send_message("%s SNR has changed from %s to %s" % (access_point["ssid"], access_point["snr"], updated_access_point["snr"]))
                     if access_point["channel"] != updated_access_point["channel"]:
                         send_message("%s channel has changed from %s to %s" % (access_point["ssid"], access_point["channel"], updated_access_point["channel"]))
-            if found == False:
+            if is_found == False:
                 send_message("%s is removed from the list" % (access_point["ssid"]))
         for updated_access_point in updated_access_points["access_points"]:
-            new = True
+            is_new = True
             for access_point in access_points["access_points"]:
                 if updated_access_point["ssid"] == access_point["ssid"]:
-                    new = False
-            if new == True:
+                    is_new = False
+            if is_new == True:
                 send_message("%s is added to the list with SNR %s and channel %s" % (updated_access_point["ssid"], updated_access_point["snr"], updated_access_point["channel"]))
         access_points = updated_access_points
 
